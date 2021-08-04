10bc89f8c92871a560a7bb4f342c6686b3730b68
Ruben Montero <rubenmontero@rocketmail.com>
2021-08-04 13:07:09 +0200

    Fix a bug

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const clock = function () {
-  let date = new Date();
+  let date = new Date("2020-04-13T03:00:00.000");
   let hrs = date.getHours();
   let mins = date.getMinutes();
   let secs = date.getSeconds();
 
-
   let period = "AM";
   if (hrs == 0) {
     hrs = 12;
-  } else if (hrs > 12) {
+  } else if (hrs == 12) {
+    hrs = 12;
+    period = "PM";
+  } else if (hrs >= 12) {
     hrs = hrs - 12;
     period = "PM";
   }
@@ -17,10 +19,9 @@ const clock = function () {
   mins = mins < 10 ? "0" + mins : mins;
   secs = secs < 10 ? "0" + secs : secs;
 
-  let time = (`${hrs}:${mins}:${secs}:${period}`);
+  let time = `${hrs}:${mins}:${secs}:${period}`;
   document.getElementById("clock").innerText = time;
   setTimeout(clock, 1000);
 };
 
 clock();
-
